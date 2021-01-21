45893a7e88565fd872f1fac2e60b880d1c28f3f8
Lifecube-devs <77251504+Lifecube-devs@users.noreply.github.com>
2021-01-21 00:26:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 //This is just a simple JS file for YouTube that creates a box on the bottom of the page
 
 var web = {
-  host: 'www.youtube.com'
+  host: 'www.youtube.com',
+  text: 'Create by WebMod'
 }
 
-if(window.location.href == web.host) {
-  var container = document.getElementById('content')
-  var box = document.createElement('div')
-  var text = document.createElement('p')
-  box.style = 'position: fixed; bottom: 0px; left: 0px; width: 100%; height: 40px; background-color: black; color: white; z-index: 999'
-  text.style = 'position: fixed; bottom: -5px; left: 40%; color: white; font-size: 40px'
-  text.innerHTML = 'This was created WebMod'
-  container.appendChild(box)
-  box.appendChild(text)
+if(window.location.href == web.host) { //Run on 'www.youtube.com' if you want to put something on every page use window.location.hostname == host name
+  var box = document.createElement('div') //Create a black box on the bottom of the page
+  var text = document.createElement('p') //Create text within that box
+  box.style = 'position: fixed; bottom: 0px; left: 0px; width: 100%; height: 40px; background-color: black; color: white; z-index: 999' //Style the box
+  text.style = 'position: fixed; bottom: -5px; left: 40%; color: white; font-size: 40px' //style the text
+  text.innerHTML = web.text //Add text to the text
+  document.body.appendChild(box) //Add the box to the body
+  box.appendChild(text) //Add the text to the box
 }
